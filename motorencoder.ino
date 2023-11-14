c36450e6e6107022863b55f4ccd6d232e9c27dc0
Dhruv <71356377+Dhruv-Raman@users.noreply.github.com>
2023-11-14 18:28:52 -0500

    Create motorencoder.ino

---
+++ motorencoder.ino
@@ -0,0 +1,111 @@
+#include <util/atomic.h> // For the ATOMIC_BLOCK macro
+
+#define ENCA 23 // YELLOW
+#define ENCB 22 // WHITE
+#define IN2 13
+#define IN1 14
+
+volatile int posi = 0; // specify posi as volatile
+long prevT = 0;
+float eprev = 0;
+float eintegral = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
+  
+  pinMode(PWM,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  
+  Serial.println("target pos");
+}
+
+void loop() {
+
+  // set target position
+  int target = 60000;
+  //int target = 250*sin(prevT/1e6);
+
+  // PID constants
+  float kp = 1;
+  float kd = 0.025;
+  float ki = 0.0;
+
+  // time difference
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+  prevT = currT;
+
+  // Read the position in an atomic block to avoid a potential
+  // misread if the interrupt coincides with this code running
+  
+  int pos = 0; 
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    pos = posi;
+  }
+  
+  // error
+  int e = pos - target;
+
+  // derivative
+  float dedt = (e-eprev)/(deltaT);
+
+  // integral
+  eintegral = eintegral + e*deltaT;
+
+  // control signal
+  float u = kp*e + kd*dedt + ki*eintegral;
+
+  // motor power
+  float pwr = fabs(u);
+  if( pwr > 255 ){
+    pwr = 255;
+  }
+
+  // motor direction
+  int dir = 1;
+  if(u<0){
+    dir = -1;
+  }
+
+  // signal the motor
+  setMotor(dir,pwr,IN1,IN2);
+
+
+  // store previous error
+  eprev = e;
+
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.println();
+}
+
+void setMotor(int dir, int pwmVal, int in1, int in2){
+  
+  if(dir == 1){
+    analogWrite(in1,pwmVal);
+    digitalWrite(in2,LOW);
+  }
+  else if(dir == -1){
+    digitalWrite(in1,LOW);
+    analogWrite(in2,pwmVal);
+  }
+  else{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+  }  
+}
+
+void readEncoder(){
+  int b = digitalRead(ENCB);
+  if(b > 0){
+    posi++;
+  }
+  else{
+    posi--;
+  }
+}
