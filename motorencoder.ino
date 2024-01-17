c854745942c16e1ab098e5f76b74aa977406ffad
Dhruv <71356377+Dhruv-Raman@users.noreply.github.com>
2024-01-16 19:18:45 -0500

    Update motorencoder.ino

--- motorencoder.ino
+++ motorencoder.ino
@@ -1,110 +1,62 @@
-#include <util/atomic.h> // For the ATOMIC_BLOCK macro
+#define DIR1 37
+#define PWM1 36
 
-#define ENCA 23 // YELLOW
-#define ENCB 22 // WHITE
-#define IN2 13
-#define IN1 14
+#define encoderPinA 23
+#define encoderPinB 22
 
-volatile int posi = 0; // specify posi as volatile
-long prevT = 0;
-float eprev = 0;
-float eintegral = 0;
+volatile long encoderCount = 0;
 
-void setup() {
+long previousTime = 0;
+float ePrevious = 0;
+float eIntegral = 0;
+
+void setup(){
   Serial.begin(9600);
-  pinMode(ENCA,INPUT);
-  pinMode(ENCB,INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
-  
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  
-  Serial.println("target pos");
+  pinMode(DIR1, OUTPUT);
+  pinMode(PWM1, OUTPUT);
+  pinMode(encoderPinA, INPUT);
+  pinMode(encoderPinB, IMPUT);
+
+  attachInterrupt(digitalPinToInterrupt(encoderPinA), handleEncoder, RISING);
 }
 
-void loop() {
+void loop(){
 
-  // set target position
-  int target = 60000;
-  //int target = 250*sin(prevT/1e6);
-
-  // PID constants
-  float kp = 1;
-  float kd = 0.025;
+  int target = 1000;
+  
+  float kp = 0.0;
+  float kd = 0.0;
   float ki = 0.0;
-
-  // time difference
-  long currT = micros();
-  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
-  prevT = currT;
-
-  // Read the position in an atomic block to avoid a potential
-  // misread if the interrupt coincides with this code running
+  float u = pidController(target, kp, kd, ki);
   
-  int pos = 0; 
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    pos = posi;
-  }
+  moveMotor(DIR1, PWM1, u);
   
-  // error
-  int e = pos - target;
-
-  // derivative
-  float dedt = (e-eprev)/(deltaT);
-
-  // integral
-  eintegral = eintegral + e*deltaT;
-
-  // control signal
-  float u = kp*e + kd*dedt + ki*eintegral;
-
-  // motor power
-  float pwr = fabs(u);
-  if( pwr > 255 ){
-    pwr = 255;
-  }
-
-  // motor direction
-  int dir = 1;
-  if(u<0){
-    dir = -1;
-  }
-
-  // signal the motor
-  setMotor(dir,pwr,IN1,IN2);
-
-
-  // store previous error
-  eprev = e;
-
   Serial.print(target);
-  Serial.print(" ");
-  Serial.print(pos);
-  Serial.println();
+  Serial.print(", ");
+  Serial.print(encoderCount);
 }
 
-void setMotor(int dir, int pwmVal, int in1, int in2){
-  
-  if(dir == 1){
-    analogWrite(in1,pwmVal);
-    digitalWrite(in2,LOW);
-  }
-  else if(dir == -1){
-    digitalWrite(in1,LOW);
-    analogWrite(in2,pwmVal);
+void handleEncoder(){
+  if(digitalRead(encoderPinA) > digitalRead(encoderPinB)){
+    encoderCount++;
   }
   else{
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-  }  
+    encoderCount--;
+  }
 }
 
-void readEncoder(){
-  int b = digitalRead(ENCB);
-  if(b > 0){
-    posi++;
+
+void moveMotor(int dirPin, int pwmPin, float u){
+  float speed = fabs(u);
+  if(speed > 255){
+    speed = 255;
   }
-  else{
-    posi--;
+  int direction = 1;
+  if(u<0){
+    direction = 0;
   }
+  digitalWrite(dirPin, direction);
+  analogWrite(pwmPin, speed);
 }
+
+float pidController(int target, //to be implemented
